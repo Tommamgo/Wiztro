294c4371efb9cde896a754f14d9ffabc97961847
the <hoppelinus@web.de>
2021-06-29 13:41:32 +0200

    macht coole Sachen wollte ich Speicher

---
+++ auto/autostart.py
@@ -0,0 +1,10 @@
+import imgkit
+def machSachen():
+    imgkit.from_url('https://wiztro.pythonanywhere.com/static/img/Profile_Logo.png', 'out.png')
+
+
+
+if __name__ == '__main__':
+    machSachen()
+
+
